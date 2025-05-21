709246769eb844db1621bb17b7a3da8016b34550
kt-eluo <kt.eluo@eluocnc.com>
2025-05-22 08:14:00 +0900

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  // reactStrictMode: true,
-  trailingSlash: true,
-  // 필요시 basePath, i18n, rewrites 등 추가 가능
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // // reactStrictMode: true,
+  // trailingSlash: true,
+  // // 필요시 basePath, i18n, rewrites 등 추가 가능
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
